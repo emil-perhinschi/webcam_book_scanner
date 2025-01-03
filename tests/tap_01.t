b32c7bacd6811a7d690c4155d2953ec115acddde
Emil Nicolaie Perhinschi <emilper@gmail.com>
2025-01-03 22:35:13 +0200

    fixes in xdg_manager.py and in tests

--- tests/tap_01.t
+++ tests/tap_01.t
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 from TAP.Simple import *
-plan(1)
+from wbs.xdg_manager import *
+
+plan(3) 
+# TODO fix tapsimple to work without a plan initialized
+# TODO add "done_testing" to tapsimple
+
+ok("/home/emilper" == get_home_path(), "home path found")
+
+# validate_xdg_data_home("test")
 
 ok(1, "one is one")
--- wbs/xdg_manager.py
+++ wbs/xdg_manager.py
@@ -19,35 +19,44 @@ XDG_CONFIG_HOME # default $HOME/.config, configuration files
 XDG_CACHE_HOME # default $HOME/.cache, cache/temporary/disposable files which can be deleted 
 """
 
+def get_home_path():
+    # GLib.get_home_dir does not work from within the VSCode terminal
+    home = GLib.get_home_dir()
+
+    if (not home):
+        home = os.getenv('HOME')
+
+    return home # TODO find other ways to guess the home if these two don't work
 
 def validate_xdg_folders(app_name: str):
     return validate_xdg_data_home(app_name) and validate_xdg_cache_home(app_name) and validate_xdg_config_home(app_name)
 
 def validate_xdg_data_home(app_name: str):
     home = get_home_path()
-    data_home = os.path.join(home, '/.local/share', app_name)
+    data_home = os.path.join(home, '.local/share', app_name)
 
-    if not os.path.isdir(data_home):
+    if (not os.path.isdir(data_home)):
         print(data_home + " not found, creating ...")
+        print(data_home)
         os.makedirs(data_home)
+
 
 def validate_xdg_config_home(app_name: str):
     home = get_home_path()
-    data_home = os.path.join(home, '/.config', app_name)
+    config_home = os.path.join(home, '.config', app_name)
 
-    if not os.path.isdir(data_home):
-        print(data_home + " not found, creating ...")
-        os.makedirs(data_home)
+    if not os.path.isdir(config_home):
+        print(config_home + " not found, creating ...")
+        os.makedirs(config_home)
 
 def validate_xdg_cache_home(app_name: str):
     home = get_home_path()
-    data_home = os.path.join(home, '/.cache', app_name)
+    cache_home = os.path.join(home, '.cache', app_name)
 
-    if not os.path.isdir(data_home):
-        print(data_home + " not found, creating ...")
-        os.makedirs(data_home)
+    if not os.path.isdir(cache_home):
+        print(cache_home + " not found, creating ...")
+        os.makedirs(cache_home)
 
-def get_home_path():
-    return GLib.get_home_dir()
 
-validate_xdg_data_home("test")
+
+
