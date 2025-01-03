14e803dba6d1704ab78277e21ca977c23fb4cd2f
Emil Nicolaie Perhinschi <emilper@gmail.com>
2025-01-03 21:01:12 +0200

    starting on TAP tests

---
+++ tests/tap_01.t
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from TAP.Simple import *
+plan(1)
+
+ok(1, "one is one")
--- wbs/xdg_manager.py
+++ wbs/xdg_manager.py
@@ -1,4 +1,6 @@
 import os, sys
+from gi.repository import GLib
+
 
 """
 TODO deal with system-wide config, data, cache folders, I guess 
@@ -22,12 +24,30 @@ def validate_xdg_folders(app_name: str):
     return validate_xdg_data_home(app_name) and validate_xdg_cache_home(app_name) and validate_xdg_config_home(app_name)
 
 def validate_xdg_data_home(app_name: str):
+    home = get_home_path()
+    data_home = os.path.join(home, '/.local/share', app_name)
 
-    return False
+    if not os.path.isdir(data_home):
+        print(data_home + " not found, creating ...")
+        os.makedirs(data_home)
 
 def validate_xdg_config_home(app_name: str):
-    return False
+    home = get_home_path()
+    data_home = os.path.join(home, '/.config', app_name)
+
+    if not os.path.isdir(data_home):
+        print(data_home + " not found, creating ...")
+        os.makedirs(data_home)
 
 def validate_xdg_cache_home(app_name: str):
-    return False
+    home = get_home_path()
+    data_home = os.path.join(home, '/.cache', app_name)
 
+    if not os.path.isdir(data_home):
+        print(data_home + " not found, creating ...")
+        os.makedirs(data_home)
+
+def get_home_path():
+    return GLib.get_home_dir()
+
+validate_xdg_data_home("test")
