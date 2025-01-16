2f850d18d997c6fd089fdc002d07065d163bbb31
Emil Nicolaie Perhinschi <emilper@gmail.com>
2025-01-16 02:45:12 +0200

    aligned the settings form with a Gtk.Grid

--- wbs/settings_gui.py
+++ wbs/settings_gui.py
@@ -28,33 +28,42 @@ class SettingsDialog(Gtk.Window):
         self.form_container.add(Gtk.Label("Settings"))
         self.add(self.form_container)
         
+        grid = Gtk.Grid()
+        self.form_container.add(grid)
+
         print("appname is " + app_name)
         config = wbs.settings.read_settings(app_name)
-
         # print(type(self.config).__name__) ConfigParser
+
+        grid_row = 0
         for section in config.sections():
-            section_box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
-            self.form_container.add(section_box)
-            section_box.add(Gtk.Label(section))
+        #     section_box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
+        #     self.form_container.add(section_box)
+        #     section_box.add(Gtk.Label(section))
+            # empty row
+            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1)
+            grid_row += 1
+            grid.attach(Gtk.Label(section), 0, grid_row, 1, 1)
+            grid_row += 1
+            # empty row
+            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1)
+            grid_row += 1
             print("Section " + section)
             for item in config[section]:
                 # print(item + " = " + config[section][item])
-                item_box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
                 item_label = Gtk.Label(item)
-                item_label.set_size_request(80, 30)
-                item_box.add(item_label)
+                item_label.set_size_request(80, 20)
+                item_label.set_xalign(0.0)
+                grid.attach(item_label, 0, grid_row, 1, 1)
                 
                 if (not section in self.form_data):
                     self.form_data[section] = {}
 
                 self.form_data[section][item] = Gtk.Entry()
                 self.form_data[section][item].set_text(config[section][item])
-                self.form_data[section][item].set_size_request(180, 30)
-                item_box.add(self.form_data[section][item])
-                section_box.add(item_box)
-    
-
-
+                self.form_data[section][item].set_size_request(500, 20)
+                grid.attach(self.form_data[section][item], 1, grid_row, 1, 1)
+                grid_row += 1
         
         self.show_all()
 
