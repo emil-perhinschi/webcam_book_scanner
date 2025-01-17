c23fca461a903ff5c696ba44fbc2d268d415d1d0
Emil Nicolaie Perhinschi <emilper@gmail.com>
2025-01-18 00:35:03 +0200

    saving the config file works

--- wbs/settings.py
+++ wbs/settings.py
@@ -1,4 +1,4 @@
-import configparser as cp
+import configparser
 import sys
 
 def init_settings_file(app_name: str):
@@ -9,7 +9,7 @@ def settings_file(app_name: str):
     pass
 
 def read_settings(app_name):
-    config = cp.ConfigParser()
+    config = configparser.ConfigParser()
 
     # config.read('/home/emilper/personal/book_scanner/wbs/tests/config.ini')
     if (not config.read('/home/emilper/personal/book_scanner/wbs/tests/config.ini')):
@@ -17,3 +17,32 @@ def read_settings(app_name):
         return False
     else:     
         return config
+
+
+def save_settings(app_name, form_data):
+    
+    print(form_data)
+
+    config = configparser.ConfigParser()
+
+    # config.read('/home/emilper/personal/book_scanner/wbs/tests/config.ini')
+    if (not config.read('/home/emilper/personal/book_scanner/wbs/tests/config.ini')):
+        print("Failed to read config file.", file=sys.stderr)
+        return False
+    
+    config = configparser.ConfigParser()
+
+    for section in form_data:
+        if (not section in config):
+            config[section] = {}
+
+        for item in form_data[section]:
+            print(section + ": " + item + " => " + form_data[section][item])
+            config[section][item] = form_data[section][item]
+
+    # TODO get the file from the xdg folders 
+    # TODO create a template of the config file
+    # TODO pick projects folder with a file picker dialog, maybe 
+    config_file_path = '/home/emilper/personal/book_scanner/wbs/tests/config.ini'
+    with open(config_file_path, 'w') as config_file:
+        config.write(config_file)
--- wbs/settings_gui.py
+++ wbs/settings_gui.py
@@ -13,59 +13,81 @@ from gi.repository import Gtk
 import wbs.settings
 
 class SettingsDialog(Gtk.Window):
-    form_data = {}
+    form_text_entries = {}
+    app_name = ""
 
 
     def __init__(self, app_name):
         Gtk.Window.__init__(self, title="Edit settings")
         # self.connect("destroy-event", lambda x: Gtk.main_quit())
+        self.app_name = app_name
+
         self.set_default_size(600,400)
         self.connect("destroy-event", self._quit_settings_dialog)
 
         self.form_container = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
         self.form_container.set_margin_top(20)
         self.form_container.set_margin_left(10)
-        self.form_container.add(Gtk.Label("Settings"))
         self.add(self.form_container)
         
         grid = Gtk.Grid()
         self.form_container.add(grid)
 
-        print("appname is " + app_name)
-        config = wbs.settings.read_settings(app_name)
+
+        self.config = wbs.settings.read_settings(self.app_name)
+        
         # print(type(self.config).__name__) ConfigParser
 
         grid_row = 0
-        for section in config.sections():
-        #     section_box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
-        #     self.form_container.add(section_box)
-        #     section_box.add(Gtk.Label(section))
-            # empty row
-            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1)
+        for section in self.config.sections():
+            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1) # empty row
             grid_row += 1
-            grid.attach(Gtk.Label(section), 0, grid_row, 1, 1)
+            section_label = Gtk.Label(section)
+            section_label.set_xalign(0.0)
+            grid.attach(section_label, 0, grid_row, 2, 1) # section label row
             grid_row += 1
-            # empty row
-            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1)
+            grid.attach(Gtk.Label(""), 0, grid_row, 1, 1) # empty row
             grid_row += 1
             print("Section " + section)
-            for item in config[section]:
-                # print(item + " = " + config[section][item])
+            if (not section in self.form_text_entries):
+                self.form_text_entries[section] = {}
+
+            for item in self.config[section]:
+                # print(item + " = " + self.config[section][item])
                 item_label = Gtk.Label(item)
                 item_label.set_size_request(80, 20)
                 item_label.set_xalign(0.0)
                 grid.attach(item_label, 0, grid_row, 1, 1)
-                
-                if (not section in self.form_data):
-                    self.form_data[section] = {}
 
-                self.form_data[section][item] = Gtk.Entry()
-                self.form_data[section][item].set_text(config[section][item])
-                self.form_data[section][item].set_size_request(500, 20)
-                grid.attach(self.form_data[section][item], 1, grid_row, 1, 1)
+                self.form_text_entries[section][item] = Gtk.Entry()
+                self.form_text_entries[section][item].set_text(self.config[section][item])
+                self.form_text_entries[section][item].set_size_request(500, 20)
+                grid.attach(self.form_text_entries[section][item], 1, grid_row, 1, 1)
                 grid_row += 1
         
+        button_box = Gtk.Box(spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
+        self.form_container.add(button_box)
+
+        save_button = Gtk.Button.new_with_mnemonic("_Save")
+        save_button.connect("clicked", self._save_settings)
+        button_box.add(save_button)
         self.show_all()
+
+    def _save_settings(self, button):
+
+        print("_save_settings called")
+
+        form_data = {}
+
+        for section in self.form_text_entries:
+            if (not section in form_data):
+                form_data[section] = {}
+            
+            for item in self.form_text_entries[section]:
+                form_data[section][item] = self.form_text_entries[section][item].get_text()
+                print(section + ": " + item + " => " + self.form_text_entries[section][item].get_text())
+        
+        wbs.settings.save_settings(self.app_name, form_data)
 
     def _quit_settings_dialog(self):
         Gtk.main_quit()
